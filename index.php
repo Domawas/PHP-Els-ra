e4bb4647ce7b1d56c7cccfa967f9d7ae3ccf4f9b
Nagy Dominik (Szf_N_2024_2026) <nagy.dominik@diak.szamalk-szalezi.hu>
2025-01-24 09:52:31 +0100

    szamok

--- index.php
+++ index.php
@@ -5,7 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP gyakorlás</title>
 </head>
-<body>
-    <?php echo "Ezt a PHP írta"; ?>
+<body> 
+    <?php
+        //kiiratás 
+        echo "<p>Ezt a PHP írta.</p>";
+        print '<p>Kiírás printtel</p>';
+        //összes eddigi változó részletes megjelenítése
+        var_dump($GLOBALS); 
+        echo "<br>";  //következő sorba kerül
+        //konstans
+        define("GREETING","Hello you! How are you today?");
+        echo constant("GREETING");
+        //változó létrehozása
+        $szoveg1 = 'szoveg1'; 
+        $szoveg2 = "szoveg2";
+        echo "<br>A két szöveg együtt1: ".$szoveg1."+".$szoveg2;
+        echo "<br>A két szöveg együtt2: $szoveg1 + $szoveg2<br>";
+        
+        $szoveg8 = "Szép";
+        $szoveg8 .=" napot!<br>"; // -  jelentése += Javaban
+        echo $szoveg8;
+        //határ keresés
+        echo pow(2.0, 1024);  //1024 az már INFINITE, arra nem ad eredményt.
+        //Ttörtek
+        echo "<br>";
+        $tort1 = 3.56;
+        $tort2 = 3.44;
+        $osszeg=$tort1+$tort2;
+        echo "<br>";
+        echo gettype($osszeg);
+        echo "<br>A két szám összege: $osszeg";
+        var_dump($GLOBALS); 
+    ?>
+    <p>Ezta a HTML írta</p>
 </body>
 </html>
