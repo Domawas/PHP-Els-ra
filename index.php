d74fafd6a8f913bfea91800126f049517413c763
Nagy Dominik (Szf_N_2024_2026) <nagy.dominik@diak.szamalk-szalezi.hu>
2025-01-24 08:53:51 +0100

    php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP gyakorlás</title>
+</head>
+<body>
+    <?php echo "Ezt a PHP írta"; ?>
+</body>
+</html>
